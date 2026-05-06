91aae7f7dcd1f87199427fa58ea306f9604989a0
fruitcookieee <yql1649@gmail.com>
2026-05-05 22:47:37 -0700

    fix next config for vercel

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -2,15 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-
-  images: {
-    unoptimized: true,
-  },
-
-  output: "export",
-
-  basePath: process.env.NODE_ENV === "production" ? "/my-app" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/my-app/" : "",
 };
 
 export default nextConfig;
